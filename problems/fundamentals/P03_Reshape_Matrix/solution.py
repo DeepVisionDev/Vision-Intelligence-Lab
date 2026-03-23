c3cf687030dfdb654ab4ea179d5b8580d755ba58
Bharat Ahirwar <137059856+DeepVisionDev@users.noreply.github.com>
2026-03-23 16:21:27 +0530

    Update solution.py

--- problems/fundamentals/P03_Reshape_Matrix/solution.py
+++ problems/fundamentals/P03_Reshape_Matrix/solution.py
@@ -1,1 +1,14 @@
+import numpy as np
 
+def reshape_matrix(a: list[list[int|float]], new_shape: tuple[int, int]) -> list[list[int|float]]:
+    
+    m = len(a)
+    n = len(a[0])
+    
+    if m * n != new_shape[0] * new_shape[1]:
+        return []
+    
+    # Using numpy reshape (logic explained in README)
+    reshaped_matrix = np.array(a).reshape(new_shape)
+    
+    return reshaped_matrix.tolist()

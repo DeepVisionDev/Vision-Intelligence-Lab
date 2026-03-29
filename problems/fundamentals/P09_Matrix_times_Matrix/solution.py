00f90d4d8834e505348f41f1f04ea33ea55dd5ed
Bharat Ahirwar <137059856+DeepVisionDev@users.noreply.github.com>
2026-03-29 16:20:48 +0530

    Update solution.py

--- problems/fundamentals/P09_Matrix_times_Matrix/solution.py
+++ problems/fundamentals/P09_Matrix_times_Matrix/solution.py
@@ -1,1 +1,20 @@
+import numpy as np
 
+def matrixmul(a: list[list[int | float]],
+              b: list[list[int | float]]) -> list[list[int | float]]:
+    
+    # Step 1: check dimension compatibility
+    a_col = len(a[0])
+    b_row = len(b)
+    
+    if a_col != b_row:
+        return -1
+    
+    # Step 2: convert to numpy arrays
+    a = np.array(a)
+    b = np.array(b)
+    
+    # Step 3: matrix multiplication
+    result = a @ b
+    
+    return result.tolist()
